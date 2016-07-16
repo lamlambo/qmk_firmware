036319e0cf833803600777812d0b936094e85f6a
lamlambo <uruhu.key@gmail.com>
2016-07-16 16:38:48 +0900

    mouse04__

--- keyboard/ergodox_ez/keymaps/default/keymap.c
+++ keyboard/ergodox_ez/keymaps/default/keymap.c
@@ -12,12 +12,6 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 1: Windows layer
- *
-*
-*
-*
-*
-*
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |   1  |   2  |   3  |   4  |   5  | `~   |           |  `~  |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
@@ -84,19 +78,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Fnctions
 [FN] = KEYMAP(
        // left hand
-       KC_NO,     KC_F1,      KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
+       KC_NO,     KC_F1,    KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,
        KC_NO,     KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
        KC_NO,     KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO,
        KC_NO,     KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
        KC_NO,     KC_NO,    KC_NO, KC_NO, KC_NO,
-                                                            KC_NO, KC_NO,
+	
+                                                              KC_NO, KC_NO,
                                                                      KC_NO,
                                                      KC_SPC, KC_ENT, KC_NO,
        // right hand
-       KC_F7, KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  RESET,
-       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO,
-              KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO,
-       TO(WINDOWS,0),  KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,
+       KC_F7, KC_F8,   KC_F9, KC_F10,  KC_F11, KC_F12, RESET,
+       KC_NO, KC_NO,   KC_NO, KC_NO,   KC_NO,  KC_NO,  KC_NO,
+              KC_NO,   KC_NO, KC_NO,   KC_NO,  KC_NO,  KC_NO,
+       TO(WINDOWS,0),  KC_NO, KC_NO,   KC_NO,  KC_NO,  KC_NO,
+	                   KC_NO, KC_NO,   KC_NO,  KC_NO,  KC_NO,
+	
        KC_NO, KC_MUTE,
        KC_NO,
        KC_NO, KC_VOLD, KC_VOLU
