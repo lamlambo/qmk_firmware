a9bbee0a133ebed92c6dd49331e4bc88bd4b55bc
lamlambo <uruhu.key@gmail.com>
2016-07-16 16:06:21 +0900

    mouse01_001

--- keyboard/ergodox_ez/keymaps/default/keymap.c
+++ keyboard/ergodox_ez/keymaps/default/keymap.c
@@ -36,17 +36,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [WINDOWS] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_NO           KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_GRV,
-        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_NO
+        KC_NO,          KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_GRV,
+        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_NO,
         KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LBRC,
         LCTL(KC_X),     LCTL(KC_V),   LCTL(KC_C),   KC_LEFT,  KC_RGHT,
-                                               KC_NO,       KC_NO,
+                                               KC_NO,         KC_NO,
                                                               KC_NO,
                                                KC_SPC,KC_BSPC,TO(MOUSE,1),
         // right hand
              KC_GRV,      KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
-             KC_NO        KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
+             KC_NO,       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
              KC_RBRC,     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
                                   KC_UP,  KC_DOWN,KC_NO,  KC_NO,            KC_NO,
@@ -122,20 +122,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Mouse
 [MOUSE] = KEYMAP(
        // left hand
-       KC_NO,     KC_F1,      KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
-       KC_NO,     KC_NO,    KC_MS_U, KC_NO, KC_NO, KC_NO, KC_NO,
-       KC_NO,     KC_MS_L,    KC_MS_D, KC_MS_R, KC_NO, KC_NO,
-       KC_NO,     KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-       KC_NO,     KC_NO,    KC_NO, KC_WH_L, KC_WH_R,
-                                                            KC_NO, KC_NO,
+       KC_NO,     KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_F6,
+       KC_NO,     KC_NO,    KC_MS_U, KC_NO,   KC_NO,   KC_NO, KC_NO,
+       KC_NO,     KC_MS_L,  KC_MS_D, KC_MS_R, KC_NO,   KC_NO,
+       KC_NO,     KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
+       KC_NO,     KC_NO,    KC_NO,   KC_WH_L, KC_WH_R,
+                                                             KC_NO,  KC_NO,
                                                                      KC_NO,
                                                      KC_SPC, KC_ENT, KC_NO,
        // right hand
-       KC_F7,         KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_NO,
-       KC_NO,         KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO,
-                      KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO,
-       TO(WINDOWS,1), KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO,
-                      KC_ACL2, KC_ACL1, KC_ACL0, KC_NO,  KC_NO,
+       KC_F7,         KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NO,
+       KC_NO,         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+       TO(WINDOWS,1), KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                      KC_ACL2, KC_ACL1, KC_ACL0, KC_NO,   KC_NO,
        KC_BTN4, KC_BTN3,
        KC_PGUP,
        KC_PGDN, KC_BTN1, KC_BTN2
